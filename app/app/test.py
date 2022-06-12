63725b9319705a361226ddd40747ae13667a8497
AlejandroCozumel <alejandroo14@gmail.com>
2022-06-11 20:29:45 -0500

    test

--- app/app/test.py
+++ app/app/test.py
@@ -2,8 +2,9 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
 
-  def test_add_number(self):
-      res = calc.add(10, 20)
-      self.assertEqual(res, 30)
+    def test_add_number(self):
+        res = calc.add(10, 20)
+        self.assertEqual(res, 30)
