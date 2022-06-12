0da7a104f3241693c8c71b8b58142a7396a5c2de
AlejandroCozumel <alejandroo14@gmail.com>
2022-06-11 20:28:41 -0500

    fff

--- app/app/test.py
+++ app/app/test.py
@@ -4,9 +4,6 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
 
-
-    def test_add_number(self):
-
+  def test_add_number(self):
       res = calc.add(10, 20)
-
       self.assertEqual(res, 30)
