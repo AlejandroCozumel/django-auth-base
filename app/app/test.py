11eaab28de6d1b663ad41c0f6beb76ab34ae0bd5
AlejandroCozumel <alejandroo14@gmail.com>
2022-06-11 20:26:27 -0500

    fin lintern

--- app/app/test.py
+++ app/app/test.py
@@ -4,8 +4,9 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
 
-  def test_add_number(self):
 
-    res = calc.add(10, 20)
+    def test_add_number(self):
 
-    self.assertEqual(res, 30)
+      res = calc.add(10, 20)
+
+      self.assertEqual(res, 30)
