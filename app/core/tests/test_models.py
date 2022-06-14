ed4620d446698736e84da48b251fdf64afdab9ac
AlejandroCozumel <alejandroo14@gmail.com>
2022-06-13 21:24:23 -0500

    se agrega admin de django y se permite hacer login por email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -37,3 +37,21 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_create_user_without_email_raises_error(self):
+        """
+        Test creating a new user without an email raises error.
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'sample123')
+
+    def test_create_superuser(self):
+        """
+        Test creating a new superuser.
+        """
+        user = get_user_model().objects.create_superuser(
+            'test1@example.com',
+            'sample123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
